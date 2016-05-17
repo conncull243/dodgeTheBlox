54be7fda8644c08f755ba5e86d5afb2871252a1f
conncull243 <conncull243@ed.amdsb.ca>
2016-05-17 10:02:34 -0400

    added two new activities, added three buttons for play, leaderboard and credits

---
+++ app/src/main/java/com/example/student/dodgetheblox/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.dodgetheblox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
